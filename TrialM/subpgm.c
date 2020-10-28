0142c4bbd9adacadd62d8afd3ecb292f8d58e8e3
nikhilakn <knnikhila@yahoo.com>
2020-10-28 10:36:19 +0530

    Trial Model added

---
+++ TrialM/subpgm.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void A (void)
+{
+        printf ("FIRST TASK\n");
+        fflush (stdout);
+}
+
+void B (void)
+{
+        printf ("SECOND TASK\n");
+        fflush (stdout);
+}
